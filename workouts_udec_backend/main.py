fb5c22319f0e5872557212d3f92f1773f4af97d1
Sebavegs2004 <167146245+Sebavegs2004@users.noreply.github.com>
2025-09-24 00:11:15 -0300

    Automatisacion almebic y crear admin + Implementacion healthcheck + Documentacion

--- workouts_udec_backend/main.py
+++ workouts_udec_backend/main.py
@@ -19,6 +19,10 @@ app.add_middleware(
 
 app.include_router(api_router, prefix="/api")
 
+@app.get("/health")
+def health_check(): # Endpoint para que Docker para chequear si el backend está vivo
+    return {"status": "ok"}
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
